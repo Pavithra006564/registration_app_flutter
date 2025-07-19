38f4b445629bead555423b2ba3f32387a8c8e1f3
Pavithra006564 <pavithra.rishikesh@gmail.com>
2025-07-19 12:58:10 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:registration_app_flutter/login.dart';
+import 'package:registration_app_flutter/registration.dart';
+import 'package:registration_app_flutter/signup.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false,
+      home: Loginpage()
+    );
+  }
+}
